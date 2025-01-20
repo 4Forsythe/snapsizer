b97284ac2a8172246ef8a698573635b1ded0212f
Forsythe <forsythe.dev@gmail.com>
2025-01-20 08:13:07 +0400

    Add seo constants

---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,3 @@
+export const SITE_NAME = 'Snapsizer'
+export const SITE_DESCRIPTION =
+  'Unlimited open-source service for optimizing and size-shifting your images (supports .JPG, .JPEG, .PNG and .WEBP)'
